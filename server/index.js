7fd95be6d1719e05639abbeb9b47621f90ab5e39
Jason Kepler <jasonkepler1@gmail.com>
2024-04-05 15:45:13 -0700

    create API endpoint: delete employee

--- server/index.js
+++ server/index.js
@@ -10,9 +10,20 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.get("/api/employees", (req, res, next) => {
-  console.log("/api/employees");
+app.get("/api/employees", (req, res) => {
+  console.log("GET: /api/employees");
   res.status(200).json(employees);
+});
+
+app.delete("/api/employees/:id", (req, res) => {
+  console.log("DELETE: /api/employees/:id");
+  const { id } = req.params;
+  const match = employees.find(emp => emp.id === Number(id));
+  if (typeof match === 'undefined') {
+    res.status(404).json({ message: "Employee not found" });
+  }
+  employees.filter(emp => emp.id !== id);
+  res.status(200).json({ message: "Employee deleted successfully" });
 });
 
 app.listen(8080, () =>
