ba17ba558f120f0243f9212ead72655820d5331c
Jason Kepler <jasonkepler1@gmail.com>
2024-04-05 13:18:05 -0700

    fetch employees from server

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [employees, setEmployees] = useState();
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/employees")
+      .then((response) => response.json())
+      .then((employees) => setEmployees(employees));
+  }, []);
+
   return (
     <div className="App">
       <h1>Plexxis Employees</h1>
+      {typeof employees === "undefined" ? <p>Loading...</p> : <p>{employees[0].id}</p>}
     </div>
   );
 }
