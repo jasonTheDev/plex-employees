086027a03be66c6ee07acc169638e068a03f5ed5
Jason Kepler <jasonkepler1@gmail.com>
2024-04-05 18:40:32 -0700

    create API endpoint: create employee

--- server/index.js
+++ server/index.js
@@ -9,6 +9,8 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.use(express.json());
+
 
 app.get("/api/employees", (req, res) => {
   console.log("GET: /api/employees");
@@ -22,8 +24,21 @@ app.delete("/api/employees/:id", (req, res) => {
   if (typeof match === 'undefined') {
     res.status(404).json({ message: "Employee not found" });
   }
-  employees.filter(emp => emp.id !== id);
+  employees.filter(emp => emp.id !== Number(id));
   res.status(200).json({ message: "Employee deleted successfully" });
+});
+
+app.post("/api/employees", (req, res) => {
+  console.log("POST: /api/employees");
+  const length = employees.length;
+  var new_id = 1;
+  if (length !== 0) {
+    // get the id of the last employee and add 1 to that
+    new_id = employees[length - 1].id + 1;
+  }
+  const new_employee = {id: new_id, ...req.body};
+  employees.push(new_employee);
+  res.status(200).json(new_employee);
 });
 
 app.listen(8080, () =>
