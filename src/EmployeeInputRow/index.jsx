66bbe2a5a0513bda7d366118f8fd78b1a66a5a8d
Jason Kepler <jasonkepler1@gmail.com>
2024-04-07 17:00:01 -0700

    make form fields required

--- src/EmployeeInputRow/index.jsx
+++ src/EmployeeInputRow/index.jsx
@@ -7,15 +7,15 @@ export const EmployeeInputRow = ({ fetchEmployees, table }) => {
     handleSubmit,
     formState: { errors },
   } = useForm({
-    defaultValues: {
-      name: "Jason Kepler",
-      code: "F100",
-      profession: "Runner",
-      color: "green",
-      city: "Victoria",
-      branch: "Tech",
-      assigned: false,
-    },
+    // defaultValues: {
+    //   name: "Jason Kepler",
+    //   code: "F100",
+    //   profession: "Runner",
+    //   color: "green",
+    //   city: "Victoria",
+    //   branch: "Tech",
+    //   assigned: false,
+    // },
   });
 
   async function createEmployee(employee) {
@@ -34,60 +34,65 @@ export const EmployeeInputRow = ({ fetchEmployees, table }) => {
     <>
       <tr id="spanned-row">
         <td colSpan={table.getCenterLeafColumns().length} align="center">
-          <span className="add-employee">Add Employee</span>
+          Add Employee
         </td>
       </tr>
-      <tr id="input-row">
+      <tr>
         <td>-</td>
-        <td>
+        <td className="input-td">
           <input
             {...register("name", { required: "Required field" })}
             placeholder="Name"
             id="name"
           />
-          <span>{errors.name?.message}</span>
+          {errors.name && <span className="form-error"> {errors.name.message}</span>}
         </td>
-        <td>
+        <td className="input-td">
           <input
-            {...register("code", { required: true })}
+            {...register("code", { required: "Required field" })}
             size="10"
             placeholder="Code"
             id="code"
           />
+          {errors.code && <span className="form-error"> {errors.code.message}</span>}
         </td>
-        <td>
+        <td className="input-td">
           <input
-            {...register("profession", { required: true })}
+            {...register("profession", { required: "Required field" })}
             size="15"
             placeholder="Profession"
             id="profession"
           />
+          {errors.profession && <span className="form-error"> {errors.profession.message}</span>}
         </td>
-        <td>
+        <td className="input-td">
           <input
-            {...register("color", { required: true })}
+            {...register("color", { required: "Required field" })}
             size="10"
             placeholder="Color"
             id="color"
           />
+          {errors.color && <span className="form-error"> {errors.color.message}</span>}
         </td>
-        <td>
+        <td className="input-td">
           <input
-            {...register("city", { required: true })}
+            {...register("city", { required: "Required field" })}
             size="15"
             placeholder="City"
             id="city"
           />
+          {errors.city && <span className="form-error"> {errors.city.message}</span>}
         </td>
-        <td>
+        <td className="input-td">
           <input
-            {...register("branch", { required: true })}
+            {...register("branch", { required: "Required field" })}
             size="15"
             placeholder="Branch"
             id="branch"
           />
+          {errors.branch && <span className="form-error"> {errors.branch.message}</span>}
         </td>
-        <td>
+        <td className="input-td">
           <select {...register("assigned")} id="assigned">
             <option value="false">No</option>
             <option value="true">Yes</option>
