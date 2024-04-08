3b921086bf8086e379219782f3da81e54f6a8035
Jason Kepler <jasonkepler1@gmail.com>
2024-04-07 23:22:30 -0700

    refactor employee.controller.js

--- server/controllers/employee.controller.js
+++ server/controllers/employee.controller.js
@@ -2,8 +2,11 @@ const { pool } = require("../data/db");
 
 const getEmployees = async (req, res) => {
   console.log("GET: /api/employees");
+
+  const selectQuery = "SELECT * FROM employees";
+  
   try {
-    const result = await pool.query("SELECT * FROM employees");
+    const result = await pool.query(selectQuery);
     res.status(200).json(result.rows);
   } catch (err) {
     console.error(err);
@@ -12,20 +15,17 @@ const getEmployees = async (req, res) => {
 };
 
 const createEmployee = async (req, res) => {
-  console.log("POST: /api/employees");
+  console.log("POST: /api/employees", { body: req.body });
+
+  const { name, code, profession, color, city, branch, assigned } = req.body;
+  const queryParams = [name, code, profession, color, city, branch, assigned];
+  const insertQuery = `
+  INSERT INTO employees (
+    name, code, profession, color, city, branch, assigned)
+    VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *`;
+    
   try {
-    const { name, code, profession, color, city, branch, assigned } = req.body;
-    console.log(
-      `Body: ${[name, code, profession, color, city, branch, assigned]}`
-    );
-
-    const result = await pool.query(
-      `INSERT INTO employees (
-        name, code, profession, color, city, branch, assigned)
-      VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *`,
-      [name, code, profession, color, city, branch, assigned]
-    );
-
+    const result = await pool.query(insertQuery, queryParams);
     res.status(201).json(result.rows[0]);
   } catch (err) {
     console.error(err);
@@ -36,17 +36,15 @@ const createEmployee = async (req, res) => {
 const deleteEmployee = async (req, res) => {
   console.log("DELETE: /api/employees/:id");
 
-  try {
-    const { id } = req.params;
-    const result = await pool.query(
-      "DELETE FROM employees WHERE id = $1 RETURNING *",
-      [id]
-    );
+  const { id } = req.params;
+  const queryParams = [id];
+  const deleteQuery = "DELETE FROM employees WHERE id = $1 RETURNING *";
 
+  try {
+    const result = await pool.query(deleteQuery, queryParams);
     if (result.rowCount === 0) {
       return res.status(404).json({ message: "Employee not found" });
     }
-
     res.status(200).json({ message: "Employee deleted successfully" });
   } catch (err) {
     console.error(err);
